6125b9ab4e86c777cb1820479cce39c93ff3038e
LoukaSoret <louka.soret@gmail.com>
2017-12-18 14:43:56 +0100

    Remplissage de create, get_size, destroy, write_byte, write_half et write_word a tester

--- memory.c
+++ memory.c
@@ -27,19 +27,32 @@ Contact: Guillaume.Huard@imag.fr
 struct memory_data {
     int size;
     int endianess;
-    uint32_t *data;
+    uint8_t *data;
 };
 
 memory memory_create(size_t size, int is_big_endian) {
-    memory mem=NULL;
+    memory mem;
+    uint32_t data[size];
+    mem = (memory) malloc(sizeof(struct memory_data));
+    if(mem == NULL){        
+        fprintf(stderr, "Error when creating simulated memory\n");
+        exit(1);
+    }
+    mem.data = data;
+    mem.size = size;
+    mem.endianess = is_big_endian;
     return mem;
 }
 
 size_t memory_get_size(memory mem) {
-    return 0;
+    return mem.size;
 }
 
 void memory_destroy(memory mem) {
+    free(mem.data);
+    mem.size=0;
+    mem.endianess=0;
+    free(mem);
 }
 
 int memory_read_byte(memory mem, uint32_t address, uint8_t *value) {
@@ -55,13 +68,23 @@ int memory_read_word(memory mem, uint32_t address, uint32_t *value) {
 }
 
 int memory_write_byte(memory mem, uint32_t address, uint8_t value) {
+    if(address >= 0 && address < mem.size){
+        mem.data[address]=value;
+        return 0;
+    }
     return -1;
 }
 
 int memory_write_half(memory mem, uint32_t address, uint16_t value) {
-    return -1;
+    int res;
+    res = memory_write_byte(mem,address,(uint8_t)(value>>sizeof(uint8_t)));
+    res |= memory_write_byte(mem,address+1,(uint8_t)((value<<sizeof(uint8_t))>>sizeof(uint8_t)));
+    return res;
 }
 
 int memory_write_word(memory mem, uint32_t address, uint32_t value) {
-    return -1;
+    int res;
+    res = memory_write_half(mem,address,(uint16_t)(value>>sizeof(uint16_t)));
+    res |= memory_write_half(mem,address+2,(uint16_t)((value<<sizeof(uint16_t))>>sizeof(uint16_t)));
+    return res;
 }
