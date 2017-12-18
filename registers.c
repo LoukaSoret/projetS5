2bd4b026c090287cc0be79104d22c61475327d28
ClockworkJackal <34648959+ClockworkJackal@users.noreply.github.com>
2017-12-18 15:23:48 +0100

    Add files via upload

--- registers.c
+++ registers.c
@@ -25,6 +25,10 @@ Contact: Guillaume.Huard@imag.fr
 #include <stdlib.h>
 
 struct registers_data {
+	uint32_t reg_no[18]; 	//16 registres + cpsr + spsr
+	uint8_t mode;					/* 7 modes : 10000 User (0)		10001 FIQ (1)	10010 IRQ (2)		10011 Supervisor (3)		10111 Abort (4)		11011 Undefined (5)		11111 System (6)
+	Privileged modes: tous sauf User
+	SPSR: tous sauf User et System*/
 };
 
 registers registers_create() {
@@ -33,48 +37,66 @@ registers registers_create() {
 }
 
 void registers_destroy(registers r) {
+ 	r = NULL;
 }
 
 uint8_t get_mode(registers r) {
-    return 0;
+    return r->mode;
 } 
 
 int current_mode_has_spsr(registers r) {
+	if (r->mode == 0 || r->mode == 6){
     return 0;
+   }
+   else{
+   	return 1;
+   }
 }
 
 int in_a_privileged_mode(registers r) {
+    if (r->mode == 0){
     return 0;
+   }
+   else{
+   	return 1;
+   }
 }
 
 uint32_t read_register(registers r, uint8_t reg) {
-    uint32_t value=0;
-    return value;
+	return r->reg_no[reg];
 }
 
 uint32_t read_usr_register(registers r, uint8_t reg) {
-    uint32_t value=0;
-    return value;
+    if(reg >= 0 && reg <= 12){
+		return r->reg_no[reg];
+	}
+	else{
+		return -1;
+	}
 }
 
 uint32_t read_cpsr(registers r) {
-    uint32_t value=0;
-    return value;
+    return r->reg_no[16];
 }
 
 uint32_t read_spsr(registers r) {
-    uint32_t value=0;
-    return value;
+    return r->reg_no[17];
 }
 
 void write_register(registers r, uint8_t reg, uint32_t value) {
+	r->reg_no[reg] = value;
 }
 
 void write_usr_register(registers r, uint8_t reg, uint32_t value) {
+	if(reg >= 0 && reg <= 12){
+		r->reg_no[reg] = value;
+	}
 }
 
 void write_cpsr(registers r, uint32_t value) {
+	r->reg_no[16] = value;
 }
 
 void write_spsr(registers r, uint32_t value) {
+	r->reg_no[17] = value;
 }
