802245339fc4ee535d2c7777c1f228facc01786e
diallama <34651525+diallama@users.noreply.github.com>
2017-12-19 11:35:06 +0100

    Add files via upload

--- arm_data_processing.c
+++ arm_data_processing.c
@@ -29,9 +29,116 @@ Contact: Guillaume.Huard@imag.fr
 
 /* Decoding functions for different classes of instructions */
 int arm_data_processing_shift(arm_core p, uint32_t ins) {
+
     return UNDEFINED_INSTRUCTION;
 }
 
 int arm_data_processing_immediate_msr(arm_core p, uint32_t ins) {
     return UNDEFINED_INSTRUCTION;
 }
+
+// Instructions de traitement de données
+
+int and_processing(uint8_t reg1,uint8_t reg2){
+   
+   //Rd := Rn AND shifter_operand
+   reg1=reg1 & reg2 ;
+
+}
+
+int sub_processing(uint8_t reg1,uint8_t reg2){
+    
+    //Rd := Rn - shifter_operand
+    reg1=reg1 - reg2;
+}
+
+int add_processing(uint8_t reg1,uint8_t reg2){
+	
+	//Rd := Rn + shifter_operand
+	reg1=reg1 + reg2;
+}
+
+int eor_processing(uint8_t reg1,uint8_t reg2){
+	
+	//Rd := Rn EOR shifter_operand
+
+	reg1=reg1 ^ reg2;
+
+}
+
+
+int rsb_processing(uint8_t reg1,uint8_t reg2){
+
+	//Rd := shifter_operand - Rn
+	reg1=reg2 - reg1;
+}
+
+int adc_processing(uint8_t reg1,uint8_t reg2,uint8_t regflag){
+
+	//Rd := Rn + shifter_operand + Carry Flag
+	reg1=reg1 - reg2 + regflag;
+}
+
+int sbc_processing(uint8_t reg1,uint8_t reg2,uint8_t regflag){
+
+	//Rd := Rn - shifter_operand - NOT(Carry Flag)
+	reg1=reg1 - reg2 - (!regflag);
+}
+
+int rsc_processing(uint8_t reg1,uint8_t reg2,uint8_t regflag){
+
+	//Rd := shifter_operand - Rn - NOT(Carry Flag)
+	reg1=reg2 - reg1 - (!regflag);
+}
+
+int tst_processing(uint8_t regflag,int flag){
+
+	//Update flags after Rn AND shifter_operand
+	regflag=flag;
+}
+
+int teq_processing(uint8_t regflag,int flag){
+
+	//Update flags after Rn EOR shifter_operand
+	regflag=flag;
+}
+
+int cmp_processing(uint8_t regflag,int flag){
+
+	//Update flags after Rn - shifter_operand
+	regflag=flag;
+}
+
+int cmn_processing(uint8_t regflag,int flag){
+
+	//Update flags after Rn + shifter_operand
+	regflag=flag;
+}
+
+int orr_processing(uint8_t regflag,int flag){
+
+	//Logical (inclusive) OR Rd := Rn OR shifter_operand
+}
+
+int mov_processing(uint8_t reg1,uint8_t reg2){
+	
+	//Rd := shifter_operand (no first operand)
+	reg1=reg2;
+}
+
+int bic_processing(){
+
+	//Bit Clear Rd := Rn AND NOT(shifter_operand)
+	reg1=reg1 & (!reg2);
+}
+
+int mvn_processing(){
+
+	//Move Not Rd := NOT shifter_operand (no first operand)
+	reg1=(!reg2);
+}
+
+
+
+
+
