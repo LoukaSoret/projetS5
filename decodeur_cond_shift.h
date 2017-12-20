354e7fa40ffa8d3184d26c62358bbebd04d7cef7
ClockworkJackal <34648959+ClockworkJackal@users.noreply.github.com>
2017-12-20 15:39:21 +0100

    Add files via upload

---
+++ decodeur_cond_shift.h
@@ -0,0 +1,12 @@
+#ifndef __DECODEUR_COND_SHIFT_H__
+#define __DECODEUR_COND_SHIFT_H__
+#include <stdint.h>
+#include "arm_core.h"
+
+int read_N(arm_core p);
+int read_Z(arm_core p);
+int read_C(arm_core p);
+int read_V(arm_core p);
+int condition(arm_core p, uint8_t cond);
+
+#endif
