4adf36dca012691fe573849048413a9f802366a4
BiancaJvRensburg <34648959+BiancaJvRensburg@users.noreply.github.com>
2017-12-21 10:54:10 +0100

    Add files via upload

--- decodeur_cond_shift.c
+++ decodeur_cond_shift.c
@@ -4,7 +4,7 @@
 #include "util.h"
 #include "debug.h"
 #include "decodeur_cond_shift.h"
-
+//
 int read_N(arm_core p){
 	uint32_t cprs = arm_read_cpsr(p);
 	
@@ -168,7 +168,78 @@ int condition(arm_core p, uint8_t cond){
 		default :
 			break;
 	
-	
+		}
+	}
+	return 0;
+}
+
+int shift(arm_core p, uint16_t ins){
+	int shift = get_bits(ins,6,4);
+	int shift_imm = get_bits(ins,11,7);
+	int8_t reg = get_bits(ins,3,0);
+	int32_t value_reg = arm_read_register(p,reg);
+	int value = 0;
+	switch(shift){
+		case LSL: // Shift left immediat
+			if(shift_imm > 0){
+				value = value_reg<<shift_imm;
+			}else{
+				value = value_reg;
+			}
+			return value;
+		break;
+
+		case LSLr: // Shift left avec registre
+			int8_t regs = get_bits(arm_read_register(p,get_bits(ins,11,8)),7,0);
+			if(regs < 32){
+				value = value_reg<<regs;
+			}else if(regs == 0){
+				value = value_reg;
+			}else{
+				value = 0;
+			}
+			return value;
+		break;
+
+		case LSR: //Shit right immediat
+			if(shift_imm > 0){
+				value = value_reg>>shift_imm;
+			}else{
+				value = value_reg;
+			}
+			return value;
+		break;
+
+		case LSRr: // Shift right avec registre
+			int8_t regs = get_bits(arm_read_register(p,get_bits(ins,11,8)),7,0);
+			if(regs < 32){
+				value = value_reg>>regs;
+			}else if(regs == 0){
+				value = value_reg;
+			}else{
+				value = 0;
+			}
+			return value;
+		break;
+
+		case ASR: // Shift arithmétique immediat
+			return asr(value_reg,shift_imm);
+		break;
+
+		case ASRr: // Shift arithmétique avec registre
+			int8_t regs = get_bits(arm_read_register(p,get_bits(ins,11,8)),7,0);
+			return ror(value_reg,regs);
+		break;
+
+		case ROR: // Rotation immediat
+			return ror(value_reg,shift_imm);
+		break;
+
+		case RORr: // rotation avec registre
+			int8_t regs = get_bits(arm_read_register(p,get_bits(ins,11,8)),7,0);
+			return ror(value_reg,regs);
+		break;
 	}
 }
+
 
--- decodeur_cond_shift.h
+++ decodeur_cond_shift.h
@@ -2,11 +2,12 @@
 #define __DECODEUR_COND_SHIFT_H__
 #include <stdint.h>
 #include "arm_core.h"
-
+//
 int read_N(arm_core p);
 int read_Z(arm_core p);
 int read_C(arm_core p);
 int read_V(arm_core p);
 int condition(arm_core p, uint8_t cond);
+int shift(arm_core p, uint16_t ins);
 
 #endif
