e5a94d2f03172189a04047fd9cdb67597fc3ddf5
BiancaJvRensburg <34648959+BiancaJvRensburg@users.noreply.github.com>
2018-01-10 12:19:51 +0100

    Add files via upload

--- decodeur_cond_shift.c
+++ decodeur_cond_shift.c
@@ -181,7 +181,7 @@ le registre d'offset Rm re registre de shift Rs et le bit I qui determine
 si le shift est execute avec shift_imm (0) ou avec Rs (1). Renvois la
 valeur de Rm shiftee.
 **************************************************************************/
-int shift(arm_core p, uint8_t shift,uint8_t shift_imm, uint8_t Rm,uint8_t Rs,uint8_t I)
+uint32_t shift(arm_core p, uint8_t shift,uint8_t shift_imm, uint8_t Rm,uint8_t Rs,uint8_t I)
 {
 	int32_t Rm_value = arm_read_register(p,Rm);
 	uint32_t Rs_value;
